f7f935cb1e4ed61312644d7de1f30562618fa791
ngngo0 <97553043+ngngo0@users.noreply.github.com>
2022-02-25 15:48:32 -0800

    setup

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -5,7 +5,8 @@ import java.nio.file.Path;
 import java.util.List;
 
 import org.junit.*;
-
+//javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
     @Test
     public void addition() {
@@ -38,6 +39,13 @@ public class MarkdownParseTest {
         Path fileName = Path.of("break3.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void lab1() throws IOException{
+        Path fileName = Path.of("snipOne.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("`google.com","google.com","ucsd.edu"),MarkdownParse.getLinks(contents));
     }
 
     
